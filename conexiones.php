cb459937273cd7b6dcc9e42070af34dcf70c3e5e
ANDERXITO <l221080133@iztapalapa.tecnm.mx>
2026-05-30 16:56:42 -0600

    Add files via upload

--- conexiones.php
+++ conexiones.php
@@ -1,13 +1,19 @@
 <?php
+// Habilitar errores por si pasa algo raro
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Conexión usando getenv() que es compatible al 100% con Railway
 $hostname = getenv('MYSQLHOST');
 $username = getenv('MYSQLUSER');
 $password = getenv('MYSQLPASSWORD');
 $database = getenv('MYSQLDATABASE');
 $port     = getenv('MYSQLPORT');
 
+// Intentar realizar la conexión a la base de datos
 $conexion = mysqli_connect($hostname, $username, $password, $database, $port);
 
 if (!$conexion) {
-    die("Error de conexión: " . mysqli_connect_error());
+    die("<h2 style='color:red; text-align:center;'>Error de conexión a la Base de Datos: " . mysqli_connect_error() . "</h2>");
 }
 ?>
